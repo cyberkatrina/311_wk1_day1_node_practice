68742912ca3f9a36233ee6f6d58244d1835e34d9
cyberkatrina <katrinaaliashkevich@utexas.edu>
2024-06-05 18:50:59 -0700

    all tests passed

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,22 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim)
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let arr = str.split("")
+  let newArray = []
+  for (i = 0; i < arr.length; i += 2) {
+    let pair = arr[i] + arr[i + 1]
+    newArray.push(pair)
+  }
+  return newArray
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  return str.split("").reverse().join("")
 }
 
 module.exports = {
